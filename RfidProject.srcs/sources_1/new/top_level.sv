b4e8e70bd4e0c4a8a0516eca078269c44a25f478
Miles J Dai <milesdai@mit.edu>
2019-11-13 17:09:38 -0500

    Inital commit with blink example

---
+++ RfidProject.srcs/sources_1/new/top_level.sv
@@ -0,0 +1,12 @@
+module top_level(
+    input clk_100mhz,
+    output logic[15:0] led
+    );
+    
+    logic [26:0] counter = 27'b0;
+    always_ff @(posedge clk_100mhz) begin
+        counter <= counter + 1;
+    end
+    
+    assign led[0] = counter[26];
+endmodule
